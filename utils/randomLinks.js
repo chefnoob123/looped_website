f3548e54e0e29c48a9ec4809db01d137774d5304
navit <navitmathur.ai23@bmsce.ac.in>
2025-05-06 16:59:05 +0530

    Fixed redirect links

--- utils/randomLinks.js
+++ utils/randomLinks.js
@@ -1,5 +1,5 @@
 export function getRandomLinks(current) {
-    const pages = ['/', '/b', '/c', '/d'].filter((p) => p !== current);
+    const pages = ['/', '/B', '/C', '/D'].filter((p) => p !== current);
     const shuffled = pages.sort(() => 0.5 - Math.random());
     return shuffled.slice(0, 3);
   }
